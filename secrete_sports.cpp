00de0254afceab3fd713e76e2fcb7df8ecf42c9c
Prakash Mondal <97402824+rko0211@users.noreply.github.com>
2023-11-15 23:53:01 +0530

    add files

---
+++ secrete_sports.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n; cin>>n;
+    string s; cin>>s;
+    cout<<s[n-1]<<endl;
+}
+int main(){
+
+int t; cin>>t;
+while (t--)
+{
+    solve();
+    /* code */
+}
+
+
+    return 0;
+}

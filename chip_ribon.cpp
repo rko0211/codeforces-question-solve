0381dd3f36335306fc7e9ecbf411b2ab7b36740b
Prakash Mondal <97402824+rko0211@users.noreply.github.com>
2023-11-26 23:35:48 +0530

    Add 26th nov file

--- chip_ribon.cpp
+++ chip_ribon.cpp
@@ -1,34 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long int 
-void solve(){
-    int n; cin>>n;
-    ll a[n]; for(int i=0;i<n;i++)cin>>a[i];
-    ll ans = 0;
-    ans+=(a[0]-1);
-    ll temp = a[0];
-    ll x = 0;
-    for(int i=1;i<n;i++){
-        if(a[i]==0){
-            ans++;
-        }
-        else if(a[i]>temp){
-          ans+=(a[i]-1-x);
-          x+=(a[i]-1-x);
-        }
-    }
-    cout<<ans<<endl;
-
-}
-int main(){
-
-int t; cin>>t;
-while (t--)
+#define ll long long int
+void solve()
 {
-    solve();
-    /* code */
+    int n;
+    cin >> n;
+    ll a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+  ll ans =0;
+  ans+=(a[0]-1);
+  for(int i=1;i<n;i++){
+    
+        ans+=max((a[i]-a[i-1]),0LL);
+    
+  }
+    cout << ans << endl;
 }
+int main()
+{
 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+        /* code */
+    }
 
     return 0;
 }

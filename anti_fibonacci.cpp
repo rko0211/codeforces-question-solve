3e39794858adcf4882f34141ca2cc65f7d61bed1
Prakash Mondal <97402824+rko0211@users.noreply.github.com>
2023-12-08 23:21:00 +0530

    Create anti_fibonacci.cpp

---
+++ anti_fibonacci.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		for (int i = 1; i <= n; ++i) {
+			cout << i;
+			for (int j = n; j > 0; --j) if (i != j)
+				cout << ' ' << j;
+			cout << '\n';
+		}
+	}
+}

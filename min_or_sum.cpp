342410e8d2b2ac6f3a634a50764f1c97f6734fa0
Prakash Mondal <97402824+rko0211@users.noreply.github.com>
2023-12-09 22:18:24 +0530

    Create min_or_sum.cpp

---
+++ min_or_sum.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int ans = 0;
+        for (int i = 0, x; i < n; ++i) {
+            cin >> x;
+            ans |= x;
+        }
+        cout << ans << endl;
+    }
+}

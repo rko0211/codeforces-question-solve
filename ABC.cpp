05cac6069d3178fb97088e69b77b349123e597dd
Prakash Mondal <97402824+rko0211@users.noreply.github.com>
2023-12-13 23:39:33 +0530

    Create ABC.cpp

---
+++ ABC.cpp
@@ -0,0 +1,23 @@
+        #include<bits/stdc++.h>
+        using namespace std;
+         
+         
+        int main() {
+            int t;
+            cin >> t;
+            while(t--) {
+                int n;
+                cin >> n;
+                string s;
+                cin >> s;
+                if(n > 2 || s == "11" || s == "00") {
+                    cout << "NO\n";
+                } else {
+                    cout << "YES\n";
+                }
+            }
+        }
+
+
+
+

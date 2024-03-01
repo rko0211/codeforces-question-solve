2696dae2bd89ecca106c5188e57c8691fd0dfd1f
Prakash Mondal <97402824+rko0211@users.noreply.github.com>
2024-03-01 22:40:56 +0530

    Add 1 march 2024 file

--- min_max.cpp
+++ min_max.cpp
@@ -1,38 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void solve(){
-    int n; cin>>n;
-    int a[n],b[n];
-    for(int i=0;i<n;i++){
-        cin>>a[i];
-    }
-    for(int i=0;i<n;i++){
-        cin>>b[i];
-    }
-    // sort(a,a+n);
-    // sort(b,b+n);
-    // int i=0;.
-    for(int j=n-1;j>=0;j--){
-        if(a[j]>b[j]){
-            swap(a[j],b[j]);
-           
-        }
-    }
-    int l1 = INT_MIN;
-    int l2 = INT_MIN;
-    for(int i=0;i<n;i++){
-        l1 = max(a[i],l1);
-        l2 = max(b[i],l2);
-    }
-    cout<<(l1*l2)<<endl;
+void solve()
+{
+  int n;
+  cin >> n;
+  int a[n];
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+  int ans = 0;
+  sort(a, a + n);
+  ans += abs(a[n - 1] - a[0]);
+  ans += abs(a[0] - a[n - 2]);
+  ans += abs(a[n - 2] - a[1]);
+  ans += abs(a[1] - a[n - 1]);
+  cout << ans << endl;
 }
-int main(){
+int main()
+{
 
-int t; cin>>t;
-while(t--){
+  int t;
+  cin >> t;
+  while (t--)
+  {
     solve();
-}
+  }
 
-
-    return 0;
+  return 0;
 }

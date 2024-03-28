3155457dadb0304a5debba33cf6c880fbcf39137
Prakash Mondal <97402824+rko0211@users.noreply.github.com>
2024-03-28 22:31:27 +0530

    Add 28th march 24 files

---
+++ nearly_short.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  if (n % 2 == 1)
+  {
+    cout << n << endl;
+    return;
+  }
+  else
+  {
+  }
+}
+int main()
+{
+
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+
+  return 0;
+}
